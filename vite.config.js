512e5d47dcf4fbab67a82676924d502d4b529ea0
Ingyu Song <ingyu3879@gmail.com>
2024-04-13 15:46:44 +0900

    :rocket: chore: 배포를 위한 설정

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import babel from "@rollup/plugin-babel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    babel({
+      babelHelpers: 'bundled',
+      exclude: 'node_modules/**', // Babel 적용 제외할 경로 설정
+      plugins: process.env.NODE_ENV === 'production' ? ['babel-plugin-transform-remove-console'] : [], // 프로덕션 환경에서만 콘솔 제거
+    }),
   ],
   resolve: {
     alias: {
